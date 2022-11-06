e8affb804402dbd7b1405c0e2bafb52aa49186da
PavelOotsutsuki <109985069+PavelOotsutsuki@users.noreply.github.com>
2022-11-06 19:41:33 +0300

    v1

---
+++ Homework1_4_3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = IntReadLine();
+            Console.WriteLine("number: " + number);
+            Console.Write("Для продолжения нажмите любую кнопку...");
+            Console.ReadKey();
+        }
+
+        static int IntReadLine()
+        {
+            bool isConvert = false;
+            int number=0;
+            Console.Write("Введите число: ");
+
+            while (isConvert == false)
+            {
+                string numberString = Console.ReadLine();
+                Console.WriteLine();
+                isConvert = int.TryParse(numberString, out number);
+
+                if (isConvert==false)
+                {
+                    Console.Write("Это не число. Попробуйте заного: ");
+                }
+            }
+
+            return number;
+        }
+
+    }
+}
